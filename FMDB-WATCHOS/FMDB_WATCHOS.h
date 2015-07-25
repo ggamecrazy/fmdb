faaf0f1bb863f725f435bf933921eed2f2672e48
Cezar Cocu <cezar@pelotoncycle.com>
2015-07-25 00:01:13 -0400

    added watchos support

---
+++ FMDB-WATCHOS/FMDB_WATCHOS.h
@@ -0,0 +1,13 @@
+//
+//  FMDB_WATCHOS.h
+//  FMDB-WATCHOS
+//
+//  Created by Cezar Cocu on 7/24/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FMDB_WATCHOS : NSObject
+
+@end
---
+++ FMDB-WATCHOS/FMDB_WATCHOS.m
@@ -0,0 +1,13 @@
+//
+//  FMDB_WATCHOS.m
+//  FMDB-WATCHOS
+//
+//  Created by Cezar Cocu on 7/24/15.
+//
+//
+
+#import "FMDB_WATCHOS.h"
+
+@implementation FMDB_WATCHOS
+
+@end
