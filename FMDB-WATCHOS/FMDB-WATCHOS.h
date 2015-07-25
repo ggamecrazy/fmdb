87c4a304014a8d03393c24d98dfaf265f99832b1
Cezar Cocu <cezar@pelotoncycle.com>
2015-07-25 00:30:07 -0400

    fixed stuff

---
+++ FMDB-WATCHOS/FMDB-WATCHOS.h
@@ -0,0 +1,19 @@
+//
+//  FMDB-WATCHOS.h
+//  FMDB-WATCHOS
+//
+//  Created by Cezar Cocu on 7/25/15.
+//
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for FMDB-WATCHOS.
+FOUNDATION_EXPORT double FMDB_WATCHOSVersionNumber;
+
+//! Project version string for FMDB-WATCHOS.
+FOUNDATION_EXPORT const unsigned char FMDB_WATCHOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FMDB_WATCHOS/PublicHeader.h>
+
+
